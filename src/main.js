df69baa95e91d206dc92a22729f86ea4548c8b31
tyo0101 <love889998@gmail.com>
2023-11-09 15:59:45 +0800

    bootstrap

--- src/main.js
+++ src/main.js
@@ -5,6 +5,9 @@ import router from './router'
 import store from './store'
 import Vue from 'vue'
 import BootstrapVue from 'bootstrap-vue'
+
+
+
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
