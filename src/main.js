a7595193c0fb6d53c2e27654853950251d9acbaf
tyo0101 <love889998@gmail.com>
2023-11-09 16:55:59 +0800

    bootstrapvue

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import store from './store'
 import Vue from 'vue'
 import BootstrapVue from 'bootstrap-vue'
 
-
+Vue.use(BootstrapVue)
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
